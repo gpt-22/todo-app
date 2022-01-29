ebc3489277cb88a654704c2a7bb69717c86db8f5
Тихомиров Григорий Павлович <gptikhomi1@mts.ru>
2022-01-29 17:09:37 +0300

    Подключение к бд из приложения

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/gtikhomiroff/todo-app/pkg/handler"
 	"github.com/gtikhomiroff/todo-app/pkg/repository"
 	"github.com/gtikhomiroff/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +17,23 @@ func main() {
 		log.Fatalf("Could not init config: %s", err)
 	}
 
-	Repository := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		DBName:   viper.GetString("db.dbname"),
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("faile to initialize db: %s", err.Error())
+	}
+
+	Repository := repository.NewRepository(db)
 	Service := service.NewService(Repository)
 	Handler := handler.NewHandler(Service)
 
